c060d96d582849a49a7f309a203731601d49a271
Arkitekt-s <sfattahieh@gmail.com>
2022-11-24 13:54:44 +0100

    crud event

--- src/main/java/com/example/gr1077data/controller/EventController.java
+++ src/main/java/com/example/gr1077data/controller/EventController.java
@@ -1,4 +1,61 @@
 package com.example.gr1077data.controller;
 
+import com.example.gr1077data.model.Event;
+import com.example.gr1077data.repo.EventRepo;
+import com.example.gr1077data.service.*;
+import com.example.gr1077data.service.exception.EventNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@AllArgsConstructor
 public class EventController {
+    private final EventService eventService;
+
+
+
+
+
+    //get all events
+    @GetMapping("/events")
+    public ResponseEntity<List<Event>> getAllEvents() throws EventNotFoundException {
+        return new ResponseEntity<>(eventService.getAllEvents(), HttpStatus.OK);
+
+    }
+    //get event by id
+    @GetMapping("/events/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable Long id) throws EventNotFoundException {
+        return new ResponseEntity<>(eventService.getEventById(id), HttpStatus.OK);
+    }
+    //creat events
+    @PostMapping("/events")
+    public ResponseEntity<Event> createEvent(@RequestBody Event newEvent) throws EventNotFoundException {
+        return new ResponseEntity<>(eventService.createEvent(newEvent), HttpStatus.CREATED);
+    }
+    @DeleteMapping("/events/{id}")
+    public ResponseEntity<Event> deleteEvent(@PathVariable Long id) throws EventNotFoundException {
+        return new ResponseEntity<>(eventService.deleteEvent(id), HttpStatus.OK);
+    }
+    @PutMapping("/events/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable Long id, @RequestBody Event event) throws EventNotFoundException {
+        return new ResponseEntity<>(eventService.updateEvent(id, event), HttpStatus.OK);
+    }
+    //search events
+    @GetMapping("/events/search")
+    public ResponseEntity<List<Event>> searchEvents(@RequestParam String search) throws EventNotFoundException {
+        return new ResponseEntity<>(eventService.searchEvent(search), HttpStatus.OK);
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/com/example/gr1077data/model/Event.java
+++ src/main/java/com/example/gr1077data/model/Event.java
@@ -1,9 +1,12 @@
 package com.example.gr1077data.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,10 +32,21 @@ public class Event {
 
     //does JPA know?
     @Column(nullable = false)
-    private LocalDateTime dateTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "HH:mm:ss")
+
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalTime endTime;
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
+
 
     @OneToOne()
     @JoinColumn(name = "image_id", referencedColumnName = "id")
